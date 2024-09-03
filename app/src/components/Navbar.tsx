4aa5d6e055ce9a171d9fee6b7f9e78efbb481f4e
randomboy24 <jatinthegod21@gmail.com>
2024-09-03 17:35:03 +0530

    ..

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 import { CartIcon } from "./CartIcon"
 import { ShoppingCart } from "./ShoppingCart"
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { ShoppingCartContext } from "../context/ShoppingCartContext"
 export const Navbar = () => {
     const [isShoppingCartOpen,setIsShoppingCartOpen] = useState(false)
+    const { items } = useContext(ShoppingCartContext);
     return (
         <div>{isShoppingCartOpen?<ShoppingCart setIsShoppingCartOpen={setIsShoppingCartOpen} />:null}
-            <div className="flex h-20 shadow-lg bg-white justify-between">
+            <div className="flex h-20 shadow-lg bg-white  border-b border-black justify-between sticky">
                 <div className="flex ">
                     <div className="flex flex-col justify-center ml-6 text-2xl ">
                         <Link to={'/'}>
@@ -24,13 +26,16 @@ export const Navbar = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center mr-10 border w-20 rounded-full border-gray-500 pl-3 h-20 cursor-pointer" onClick={() => setIsShoppingCartOpen(true) }>
-                    <div className="bg-red-600 h-6 w-[25px] absolute top-auto ml-7 mt-16 rounded-full mr-2 text-white pl-2
-                    pr-1 border-white font-semibold">
-                        3
-                    </div>
-                    <div>
-                        <CartIcon />
+                <div className="pt-[3px]">  
+                    <div className="flex flex-col justify-center mr-10 border w-[75px]
+                        rounded-full border-gray-500 pl-3 h-[75px] cursor-pointer" onClick={() => setIsShoppingCartOpen(true) }>
+                        <div className="bg-red-600 h-[24px] w-[24px] absolute top-auto ml-7 mt-16 rounded-full mr-2 text-white pl-2
+                        pr-1 border-white font-semibold">
+                            {(items.filter(item => item.quantity>0)).length}
+                        </div>
+                        <div>
+                            <CartIcon />
+                        </div>
                     </div>
                 </div>
             </div>
